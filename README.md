0efdee5596c18b7e7a4bb35d5eab970aa8cd06d9
SaishJ <saishjagtap0003@gmail.com>
2022-09-16 12:24:33 +0530

    :memo: useState with previous state
    useState with previous state

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React Hooks
 
-### [useState]()
+### [useState](https://github.com/SaishJ/React-Hooks/commit/a04d9fc28e67ee838856cfbcecef87f99409c25c)
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import CounterTwo from "./components/CounterTwo";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <CounterTwo />
     </div>
   );
 }
